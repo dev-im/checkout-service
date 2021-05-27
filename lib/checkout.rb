965355524adff956b678db1c985f506a884f66eb
Iliana Meza <ilianameza530@gmail.com>
2021-05-27 11:47:33 +0100

    Ckeckout class can now take promotional_rules as an argument There are two promotions rules at the moment Promotional rules are flexible meaning: -there can be as many promotional rules as wanted -the rules of the promotions (ie. values) can be input differently  without affecting the checkout process. Changed item codes to be strings and item prices to be floating points

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,6 +1,7 @@
 class Checkout 
 
-    def initialize
+    def initialize(promotional_rules)
+        @promotional_rules = promotional_rules
         @basket = []
     end
 
@@ -9,13 +10,53 @@ class Checkout
     end
 
     def total
-        total_sum
+        discounted_basket = apply_product_promos
+        total = total_sum(discounted_basket)
+        apply_order_promo(total)
     end
 
     private
 
-    def total_sum
-        item_prices = @basket.map { |k| k[:price] }
-        item_prices.sum/100.to_f
+    def total_sum(discounted_basket)
+        item_prices = discounted_basket.map { |k| k[:price] }
+        item_prices.sum
+    end
+
+    def apply_order_promo(total)
+        matching_promo = order_total_promos.find { |promo| total >= promo[:total] }
+        total = total - (total * matching_promo[:discount_percentage] / 100).floor(2) if matching_promo
+        total  
+    end
+
+    def apply_product_promos
+        promotions_to_apply = valid_product_promos
+        new_basket = @basket.map do |product|
+            product[:price] = promotions_to_apply[product[:code]] if promotions_to_apply.key?(product[:code])
+            product
+        end
+        new_basket
+    end
+
+    def valid_product_promos
+        promos = {}
+        basket_quantities.each do |basket_code, basket_quantity| 
+            matching_promo = product_promos.find { |promo| promo[:product_code] == basket_code }
+            if matching_promo && basket_quantity >= matching_promo[:quantity]
+                promos[basket_code] = matching_promo[:discount_price]
+            end
+        end
+        promos
+    end
+
+    def basket_quantities
+        @basket.map { |product| product[:code] }.group_by(&:itself).transform_values(&:count)
+    end
+
+    def order_total_promos
+        @promotional_rules.filter { |promo| promo[:promo_type] == "total"}
+    end
+
+    def product_promos
+        @promotional_rules.filter { |promo| promo[:promo_type] == "product"}
     end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,34 +1,52 @@
 require_relative '../lib/checkout.rb'
 
 describe Checkout do
-    subject(:checkout) { described_class.new }
-    let(:item_one) { Product.new(code: 001, name: 'Lavender heart', price: 925) }
-    let(:item_two) { Product.new(code: 002, name: 'Personalised cufflinks', price: 4500) }
-    let(:item_three) { Product.new(code: 003, name: 'Kids T-shirt', price: 1995) }
+    subject(:checkout) { described_class.new(promotional_rules) }
+    let(:order_total_promo) { {promo_type: "total", total: 60, discount_percentage: 10} }
+    let(:product_promo) { {promo_type: "product", quantity: 2, product_code: '001', discount_price: 8.50} }
+    let(:promotional_rules) { [order_total_promo, product_promo] }
+    let(:item_one) { Product.new(code: '001', name: 'Lavender heart', price: 9.25) }
+    let(:item_two) { Product.new(code: '002', name: 'Personalised cufflinks', price: 45.00) }
+    let(:item_three) { Product.new(code: '003', name: 'Kids T-shirt', price: 19.95) }
 
     describe '#scan' do
         it 'pushes an item into a basket' do
-            expect(checkout.scan(item_one)).to eq [{code: 001, price: 925}]
+            expect(checkout.scan(item_one)).to eq [{code: '001', price: 9.25}]
         end
 
         it 'pushes multiple items into a basket' do
             checkout.scan(item_two)
-            expect(checkout.scan(item_three)).to eq [{code: 002, price: 4500}, {code: 003, price: 1995}]
+            expect(checkout.scan(item_three)).to eq [{code: '002', price: 45.00}, {code: '003', price: 19.95}]
         end
     end
 
     describe '#total' do
-        it 'calculates the total sum of two items in the basket' do
+        it 'calculates the total sum of two items in the basket with no promotion' do
             checkout.scan(item_one)
             checkout.scan(item_two)
             expect(checkout.total).to eq(54.25)
         end
 
-        it 'calculates the total sum of three items in the basket' do
+        it 'applies the order total promo' do
             checkout.scan(item_one)
             checkout.scan(item_two)
             checkout.scan(item_three)
-            expect(checkout.total).to eq(74.20)
+            expect(checkout.total).to eq(66.78)
+        end
+
+        it 'applies the product promo' do
+            checkout.scan(item_one)
+            checkout.scan(item_three)
+            checkout.scan(item_one)
+            expect(checkout.total).to eq(36.95)
+        end
+
+        it 'applies the order total promo and the product promo' do
+            checkout.scan(item_one)
+            checkout.scan(item_two)
+            checkout.scan(item_one)
+            checkout.scan(item_three)
+            expect(checkout.total).to eq(73.76)
         end
     end
 end
